8e760dd20a84850ddaea82d650e833947e9d73ea
t ddddddd <miscmisc@cmme.org>
2013-10-24 10:40:28 -0700

    Use new RSpec expectation syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
+require "fakeredis/rspec"
+require "feature_guard"
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
   config.order = 'random'
+
+  config.expect_with(:rspec) do |c|
+    c.syntax = :expect
+  end
 end
